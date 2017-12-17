8578f52b198dd595010cbeb69b5ee68822e68988
Austin Bailey <baileyau@bc.edu>
2017-12-17 09:53:36 -0500

    added table function and gettype function

--- Query.php
+++ Query.php
@@ -5,31 +5,70 @@ class Query {
 	public $mysqli = null;
 	public $sql_command = "";
 	public $sql_info = "";
-	public $datatypes = "";
+	public $datatypes = array();
 	public $data = array();
+	public $table = "";
 
-	function __construct($mysqli = null) {
+	public function __construct($mysqli = null) {
 		// construct with mysqli object
 		$this->mysqli = $mysqli;
 	}
 
-	function set_mysqli($mysqli) {
+	public function set_mysqli($mysqli) {
 		$this->mysqli = $mysqli;
 	}
 
-	function clear() {
+	public function clear() {
 		//clears all Query object data
 		$this->sql_command = '';
 		$this->sql_info = '';
 		$this->datatypes = '';
 		$this->data = array();
+		$this->table = '';
+	}
+
+	public function table($table) {
+		$this->table = $table;
+
+		$this->getTypes($table);
+
+		return $this;
+	}
+
+	private function getTypes($table) {
+
+		// fetch describe array of this table
+		$sql = "DESCRIBE $table";
+		$stmt = $this->mysqli->prepare($sql);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$data = array();
+		while ($row = $result->fetch_assoc()) {
+			$data[] = $row;
+		}
+
+		//build array to know type of each column in the table. this removes the need for user-input types
+		foreach ($data as $row) {
+			if (strpos($row['Type'], 'int') > -1) {
+				$type = 'i';
+			} else if (strpos($row['Type'], 'double') > -1 || strpos($row['Type'], 'float') > -1) {
+				$type = 'd';
+			} else if (strpos($row['Type'], 'varchar') > -1) {
+				$type = 's';
+			} else {
+				$type = 'b';
+			}
+
+			$this->datatypes[$row['Field']] = $type;
+		}
+
 	}
 
 	/*
 		****SELECT****
 	*/
 
-	function select($table, ...$columns) {
+	public function select($table, ...$columns) {
 		/*
 		* Resets and sets the command to SELECT $columns FROM $table
 		*/
@@ -52,16 +91,16 @@ class Query {
 
 	}
 
-	function where($column, $condition, $value, $type, $logic = "AND") {
+	public function where($column, $condition, $value, $type, $logic = "AND") {
 		/*
-		* Append conditions for WHERE statements.
-		* Ex: WHERE count > 0
-		* $type is to be either 'i' for int, 's', for string, 'd' for double, or 'b' for blob
-		* By default, $logic is 'AND'. User can specify how they want to connect conditions. 
-		* If it is the first condition, $logic is ignored 
+		Append conditions for WHERE statements.
+		Ex: WHERE count > 0
+		$type is to be either 'i' for int, 's', for string, 'd' for double, or 'b' for blob
+		By default, $logic is 'AND'. User can specify how they want to connect conditions. 
+		If it is the first condition, $logic is ignored 
 		*/
 
-		if (!strpos($this->sql_info, "WHERE")) {
+		if (!(strpos($this->sql_info, "WHERE") > -1)) {
 			$this->sql_info = "WHERE $column $condition ?"; 
 			$this->data = array();
 			$this->data[] = $value;
@@ -75,25 +114,34 @@ class Query {
 		return $this;
 	}
 
-	function order_by($column, $order) {
+	public function or_where() {
+
+	}
+
+	public function and_where() {
+
+	}
+
+	public function order_by($column, $order) {
 		//order query by $column in order of $order
 		$this->sql_info = $this->sql_info . " ORDER BY $column $order"; 
 
 		return $this;
 	}
 
-	function limit($n) {
+	public function limit($n) {
 		$this->sql_info = $this->sql_info . " LIMIT $n";
 
 		return $this;
 	}
 
-	function get() {
+	public function get() {
 		/*
 		Returns array of selected rows
 		*/
 
 		$sql = "$this->sql_command $this->sql_info";
+		var_dump($sql);
 		$stmt = $this->mysqli->prepare($sql);
 		$stmt->bind_param($this->datatypes, ...$this->data);
 		$stmt->execute();
@@ -112,7 +160,7 @@ class Query {
 		****INSERT****
 	*/
 
-	function insert($table, ...$columns) {
+	public function insert($table, ...$columns) {
 		$sql_command = "INSERT INTO $table ($columns[0]";
 		for ($i=1; $i < count($columns); $i++) { 
 			$sql_command = $sql_command . ", " . $columns[$i];
@@ -125,7 +173,7 @@ class Query {
 		return $this;
 	}
 
-	function values($datatypes, ...$values) {
+	/*function values($datatypes, ...$values) {
 		$sql_info = "VALUES (?";
 		//reset data array in case it has been used
 		$this->data = array();
@@ -144,9 +192,9 @@ class Query {
 
 		return $this;
 
-	}
+	}*/
 
-	function exec_insert() {
+	public function exec_insert() {
 		$sql = "$this->sql_command $this->sql_info";
 		$stmt = $this->mysqli->prepare($sql);
 		$stmt->bind_param($this->datatypes, ...$this->data);
@@ -165,14 +213,14 @@ $mysqli = new mysqli($servername, $username, $password, $dbname);
 
 $query = new Query($mysqli);
 
-$myData = $query->select('users', '*')
+/*$myData = $query->select('users', '*')
+				->where('name', '=', 'Austin Bailey', 's')
+				->where('id', '=', "egag", 's', 'OR')
 				->order_by('id', 'DESC')
 				->limit(5)
-				->get();
+				->get();*/
 
-var_dump($myData);
-
-
+$query->table('lifts');
 
 
 
